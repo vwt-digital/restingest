11122f0d76509590ee86486dd6f23a0a800b8081
Bernie van Veen <b.vanveen@vwt.digital>
2019-04-03 08:46:07 +0200

    Fixed test config. CAR-10

---
+++ functions/config.example.py
@@ -0,0 +1,6 @@
+# Uncomment and specify to store to Azure cloud storage
+#   AZURE_STORAGE_ACCOUNT = 'storageaccountname'
+#   AZURE_STORAGE_KEY = '***'
+#   AZURE_STORAGE_CONTAINER = 'containername'
+
+BASE_PATH = 'base/path'
