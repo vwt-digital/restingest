9fe9762572a684134f625f02dce00adf2ac2b1c6
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-04-24 13:00:17 +0200

    Revert "Extend restingest to handle POST. CAR-43"
    This reverts commit c1a40792

--- functions/openapi_server/controllers/default_controller.py
+++ functions/openapi_server/controllers/default_controller.py
@@ -21,11 +21,3 @@ def generic_post(body):
 
 def generic_post2(body):
     return generic_post(body)
-
-
-def generic_post3(body):
-    return generic_post(body)
-
-
-def generic_post4(body):
-    return generic_post(body)
