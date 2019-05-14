3757b7c1cdad5af59adab032f4d93ca5625a1fc3
Niels Kamp <n.kamp@developers.nl>
2019-05-14 13:41:24 +0200

    Fixed server crash. DAT-30

--- functions/openapi_server/controllers/security_controller_.py
+++ functions/openapi_server/controllers/security_controller_.py
@@ -11,9 +11,12 @@ if hasattr(config, 'OAUTH_JWKS_URL'):
 
 def refine_token_info(token_info):
     if token_info and 'appid' in token_info:
-        for appid_info in config.OAUTH_APPID:
-            if token_info['appid'] == appid_info['appid']:
-                token_info['scopes'] = appid_info['scopes']
+        try:
+            for appid_info in config.OAUTH_APPID:
+                if token_info['appid'] == appid_info['appid']:
+                    token_info['scopes'] = appid_info['scopes']
+        except NameError:
+            pass
     return token_info
 
 
