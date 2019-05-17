f046453111abba35a1111c74edff1f8679c0b8af
P. Mutale <peter@mutale.nl>
2019-05-17 11:24:40 +0200

    Twitch data to be presentable DAT-64

--- functions/surveys/csv/helpers.py
+++ functions/surveys/csv/helpers.py
@@ -10,11 +10,15 @@ logger = logging.getLogger(__name__)
 
 def create_csv_file(surveys):
     """
-    Creates the csv file that gets downloaded on request
+    Creates the csv file that gets downloaded on request. on only the data
     :param surveys:
     :return:
     """
-    return pd.DataFrame(surveys).to_csv()
+    data = dict()
+    for item in surveys:
+        data[surveys[item]["id"]] = surveys[item]["data"]
+    df = pd.DataFrame(json.loads(json.dumps(data))).transpose()
+    return df.to_csv()
 
 
 def get_batch_surveys(bucket_name):
@@ -22,12 +26,14 @@ def get_batch_surveys(bucket_name):
         lists all the surveys in the bucket
         - Using the a stringgetter() - batch them into a single dict file
     """
-    storage_client = storage.Client(os.environ.get('PROJECT', 'Specified environment variable is not set.'))
+    storage_client = storage.Client(
+        os.environ.get("PROJECT", "Specified environment variable is not set.")
+    )
 
     bucket = storage_client.get_bucket(bucket_name)
 
     latest = list(bucket.list_blobs())[-1].download_as_string()
-    logger.info('Downloaded Survey String', latest)
+    logger.info("Downloaded Survey String", latest)
     return json.loads(latest)
 
 
@@ -40,5 +46,5 @@ def get_presentable_surveys(elements):
     """
     surveys = dict()
     for survey in elements:
-        surveys[survey['id']] = survey
-    return pd.DataFrame(surveys).to_json()
+        surveys[survey["id"]] = survey
+    return surveys
