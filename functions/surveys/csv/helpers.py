759e58a3380bcc0d1540179e8e50516e18856e13
P. Mutale <peter@mutale.nl>
2019-05-20 11:50:04 +0200

    Flat subquestion to a 2 dimensional prep DAT-77

--- functions/surveys/csv/helpers.py
+++ functions/surveys/csv/helpers.py
@@ -8,16 +8,63 @@ from google.cloud import storage
 logger = logging.getLogger(__name__)
 
 
+def flatten_dict(value):
+    """
+    Flattens a dictionary object with (nested) list values. For example:
+    a_registration:  another_list_registration: {
+                    _tt667dsfs: [ x: { y: fs56df64sd3} ]
+                    }
+
+    => { a_registration.another_list_registration._tt667dsfs.x.y: 'fs56df64sd3' }
+    :param value:
+    :return:
+    """
+    flat_dict__ = dict()
+    for _key, _value in value.items():
+        if isinstance(_value, list):
+            torn = dict()
+            for index, x in enumerate(_value):
+                torn[f"items__{index}"] = pd.io.json.json_normalize(
+                    x, sep="."
+                ).to_dict(orient="records")[0]
+                flat_dict__[_key] = torn
+        else:
+            flat_dict__[_key] = _value
+    return flat_dict__
+
+
 def create_csv_file(surveys):
     """
-    Creates the csv file that gets downloaded on request. on only the data
+    Creates the csv file that gets downloaded exclusively data on request.
+    And flatten sub question to a 2 dimensional data representation
+    ** for example:
+    locationSearch: {
+        mast: 'x3srrR'
+        typea: 'A type'
+        another: [ anotherList: {
+                    _tt667dsfs: fs56df64sd3}
+                    ]
+        }
+    => locationSearch.mast: 'x3srrR' ...
+    => locationSearch.another.anotherList._tt667dsfs: 'fs56df64sd3' ...
     :param surveys:
     :return:
     """
     data = dict()
-    for item in surveys:
-        data[surveys[item]["id"]] = surveys[item]["data"]
-    df = pd.DataFrame(json.loads(json.dumps(data))).transpose()
+    for k, v in surveys.items():
+        for key, value in v["data"].items():
+            if isinstance(value, list):
+                flat_list = pd.DataFrame(value).to_dict(orient="records")[0]
+                data[key] = pd.io.json.json_normalize(flat_list, sep=".").to_dict(
+                    orient="records"
+                )[0]
+            elif isinstance(value, dict):
+                data[key] = flatten_dict(value)
+            else:
+                data[key] = value
+
+    data = pd.io.json.json_normalize(data, sep=".").to_dict(orient="records")[0]
+    df = pd.DataFrame(json.loads(json.dumps(data)), index=[0]).transpose()
     return df.to_csv()
 
 
