a8f00abf88d2aafeca8c9762613471a85db00970
Bernie van Veen <b.vanveen@vwt.digital>
2019-09-30 10:44:38 +0200

    Use leading zeros in date-path. DAT-962

--- functions/openapi_server/controllers/default_controller.py
+++ functions/openapi_server/controllers/default_controller.py
@@ -27,7 +27,7 @@ def generic_post(body):
     now = datetime.datetime.utcnow()
     timestamp = '%04d%02d%02dT%02d%02d%02dZ' % (now.year, now.month, now.day,
                                                 now.hour, now.minute, now.second)
-    destinationpath = '%s%s/%d/%d/%d/%s.json' % (current_app.base_path, request.path, now.year, now.month, now.day,
+    destinationpath = '%s%s/%04d/%02d/%02d/%s.json' % (current_app.base_path, request.path, now.year, now.month, now.day,
                                                  timestamp)
     for cs in current_app.cloudstorage:
         cs.storeBlob(destinationpath, json.dumps(apply_pii_filter(copy.deepcopy(body),
