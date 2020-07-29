70d03ee250fc7fed4f560f72692095c96c5fe5f0
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-07-29 10:56:06 +0200

    Changed env variables DAT-3709

--- e2e_test/gcloud/get.config.py
+++ e2e_test/gcloud/get.config.py
@@ -1,5 +1,5 @@
 import os
-GOOGLE_STORAGE_BUCKET = str(os.environ['_STORAGE_BUCKET'])
+GOOGLE_STORAGE_BUCKET = str(os.environ['STORAGE_BUCKET'])
 
 # Base path of blobs stored
 BASE_PATH = 'test/e2e'
