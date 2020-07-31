2e3f8100304f4131045bff6d746eef2aa1d2556a
David Strootman <david.strootman@gmail.com>
2020-07-31 10:42:51 +0200

    DAT-3708: Add post tests

--- e2e_test/test.py
+++ e2e_test/test.py
@@ -1,6 +1,43 @@
 import unittest
+import requests
 
 
 class E2ETest(unittest.TestCase):
     def test_sunny_day(self):
-        self.assertTrue(5 or -2)
+        self.assertTrue(0xDEADBEEF)
+
+    def test_post_json_no_auth_generic_pos(self):
+        payload = {
+            'ID': 0
+        }
+
+        r = requests.post('https://europe-west1-vwt-d-gew1-dat-restingest-test.cloudfunctions.net/vwt-d-gew1-dat'
+                          '-restingest-test-receive-ingest-func/store-json', json=payload)
+        try:
+            self.assertTrue(199 < r.status_code < 300)
+        except AssertionError as e:
+            import sys
+            raise type(e)(str(e) + "\n\n Full response:\n" + r.text).with_traceback(sys.exc_info()[2])
+
+    def test_post_json_no_auth_generic_neg(self):
+        r = requests.post('https://europe-west1-vwt-d-gew1-dat-restingest-test.cloudfunctions.net/vwt-d-gew1-dat'
+                          '-restingest-test-receive-ingest-func/store-json')
+        try:
+            self.assertFalse(199 < r.status_code < 300)
+        except AssertionError as e:
+            import sys
+            raise type(e)(str(e) + "\n\n Full response:\n" + r.text).with_traceback(sys.exc_info()[2])
+
+    # def test_post_json_no_auth_schema_neg(self):
+    #     payload = {
+    #         'ID': 0,
+    #         'superfluous': "This is one more argument than the openapi description accepts"
+    #     }
+    #
+    #     r = requests.post('https://europe-west1-vwt-d-gew1-dat-restingest-test.cloudfunctions.net/vwt-d-gew1-dat'
+    #                       '-restingest-test-receive-ingest-func/store-json', json=payload)
+    #     try:
+    #         self.assertFalse(199 < r.status_code < 300)
+    #     except AssertionError as e:
+    #         import sys
+    #         raise type(e)(str(e) + "\n\n Full response:\n" + r.text).with_traceback(sys.exc_info()[2])
