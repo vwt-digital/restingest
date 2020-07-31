1a6e20f23259f12b9a2dd53ee8d7920e815c8800
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-07-31 11:34:31 +0200

    Added domain to test DAT-3709

--- e2e_test/test.py
+++ e2e_test/test.py
@@ -1,10 +1,27 @@
 import unittest
 import requests
+import sys
 
 
 class E2ETest(unittest.TestCase):
+    _domain = ''
+
     def test_sunny_day(self):
         self.assertTrue(0xDEADBEEF)
+
+    def test_get_json_stg_store_generic(self):
+        payload = {
+            'geturl': 'generics'
+        }
+        r = requests.post(f'https://europe-west1-{self._domain}.cloudfunctions.net/'
+                          f'{self._domain}-request-ingest-func?geturl=generics', json=payload)
+        print(r.text)
+        print(r.content)
+        try:
+            self.assertTrue(199 < r.status_code < 300)
+        except AssertionError as e:
+            import sys
+            raise type(e)(str(e) + "\n\n Full response:\n" + r.text).with_traceback(sys.exc_info()[2])
 
     def test_post_json_no_auth_generic_pos(self):
         payload = {
@@ -41,3 +58,12 @@ class E2ETest(unittest.TestCase):
     #     except AssertionError as e:
     #         import sys
     #         raise type(e)(str(e) + "\n\n Full response:\n" + r.text).with_traceback(sys.exc_info()[2])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 1:
+        print('No domain arguments found')
+        exit(1)
+
+    E2ETest._domain = sys.argv.pop()
+    unittest.main()
