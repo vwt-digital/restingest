8722ea653305d2769c0c76ac3fa2699f96e6f7c6
David Strootman <david.strootman@gmail.com>
2020-08-04 09:05:31 +0200

    DAT-3708: Allow unauthenticated for testing post

--- e2e_test/test.py
+++ e2e_test/test.py
@@ -26,21 +26,24 @@ class E2ETest(unittest.TestCase):
     #         import sys
     #         raise type(e)(str(e) + "\n\n Full response:\n" + r.text).with_traceback(sys.exc_info()[2])
 
-    # def test_post_json_no_auth_generic_pos(self):
-    #     payload = {
-    #         'ID': 0
-    #     }
-    #
-    #     r = requests.post('https://europe-west1-vwt-d-gew1-dat-restingest-test.cloudfunctions.net/vwt-d-gew1-dat'
-    #                       '-restingest-test-receive-ingest-func/store-json', json=payload)
-    #     try:
-    #         self.assertTrue(199 < r.status_code < 300)
-    #     except AssertionError as e:
-    #         raise type(e)(str(e) + "\n\n Full response:\n" + r.text)
+    def test_post_json_no_auth_generic_pos(self):
+        params = {
+            'storepath': 'store_json'
+        }
+        payload = {
+            'ID': 0
+        }
+
+        r = requests.post('https://europe-west1-' + self._domain + '.cloudfunctions.net/' + self._domain +
+                          '-receive-ingest-func', params=params, json=payload)
+
+        try:
+            self.assertTrue(199 < r.status_code < 300)
+        except AssertionError as e:
+            raise type(e)(str(e) + "\n\n Full response:\n" + r.text)
 
     # def test_post_json_no_auth_generic_neg(self):
-    #     r = requests.post('https://europe-west1-vwt-d-gew1-dat-restingest-test.cloudfunctions.net/vwt-d-gew1-dat'
-    #                       '-restingest-test-receive-ingest-func/store-json')
+    #     r = requests.post('https://europe-west1-' + self._domain + '.cloudfunctions.net/' + self._domain + '-receive-ingest-func', params=params, json=payload)
     #     try:
     #         self.assertFalse(199 < r.status_code < 300)
     #     except AssertionError as e:
@@ -52,8 +55,8 @@ class E2ETest(unittest.TestCase):
     #         'superfluous': "This is one more argument than the openapi description accepts"
     #     }
     #
-    #     r = requests.post('https://europe-west1-vwt-d-gew1-dat-restingest-test.cloudfunctions.net/vwt-d-gew1-dat'
-    #                       '-restingest-test-receive-ingest-func/store-json', json=payload)
+    #     r = requests.post('https://europe-west1-' + self._domain + '.cloudfunctions.net/' + self._domain + '-receive-ingest-func', params=params, json=payload)
+
     #     try:
     #         self.assertFalse(199 < r.status_code < 300)
     #     except AssertionError as e:
