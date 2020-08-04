53b9c5a8af29e11c82bc9d46d82ca2c42c7d6da2
David Strootman <david.strootman@gmail.com>
2020-08-04 12:25:40 +0200

    DAT-3708: Try making JSON schema validation work

--- e2e_test/test.py
+++ e2e_test/test.py
@@ -52,7 +52,7 @@ class E2ETest(unittest.TestCase):
 
     def test_post_json_no_auth_schema_neg(self):
         payload = {
-            'NOT_ID': 3
+            'NOT_ID': "test"
         }
 
         r = requests.post('https://europe-west1-' + self._domain + '.cloudfunctions.net/' + self._domain +
