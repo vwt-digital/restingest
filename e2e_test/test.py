81d0b3b0d7be5886ed816003d975821c39d04dba
David Strootman <david.strootman@gmail.com>
2020-08-04 11:20:29 +0200

    DAT-3708: Fix post urls

--- e2e_test/test.py
+++ e2e_test/test.py
@@ -4,7 +4,6 @@ import os
 
 
 class E2ETest(unittest.TestCase):
-
     _domain = os.environ["domain"]
 
     def test_sunny_day(self):
@@ -17,7 +16,8 @@ class E2ETest(unittest.TestCase):
     #     payload = {
     #         'geturl': 'generics'
     #     }
-    #     r = requests.post('https://europe-west1-' + self._domain + '.cloudfunctions.net/' + self._domain + '-request-ingest-func', params=params, json=payload)
+    #     r = requests.post('https://europe-west1-' + self._domain + '.cloudfunctions.net/' + self._domain +
+    #                       '-request-ingest-func', params=params, json=payload)
     #     print(r.text)
     #     print(r.content)
     #     try:
@@ -27,38 +27,40 @@ class E2ETest(unittest.TestCase):
     #         raise type(e)(str(e) + "\n\n Full response:\n" + r.text).with_traceback(sys.exc_info()[2])
 
     def test_post_json_no_auth_generic_pos(self):
-        params = {
-            'storepath': 'store_json'
-        }
         payload = {
             'ID': 0
         }
 
         r = requests.post('https://europe-west1-' + self._domain + '.cloudfunctions.net/' + self._domain +
-                          '-receive-ingest-func', params=params, json=payload)
+                          '-receive-ingest-func/store-json', json=payload)
 
         try:
             self.assertTrue(199 < r.status_code < 300)
         except AssertionError as e:
             raise type(e)(str(e) + "\n\n Full response:\n" + r.text)
 
-    # def test_post_json_no_auth_generic_neg(self):
-    #     r = requests.post('https://europe-west1-' + self._domain + '.cloudfunctions.net/' + self._domain + '-receive-ingest-func', params=params, json=payload)
-    #     try:
-    #         self.assertFalse(199 < r.status_code < 300)
-    #     except AssertionError as e:
-    #         raise type(e)(str(e) + "\n\n Full response:\n" + r.text)
+    def test_post_json_no_auth_generic_neg(self):
+        payload = {
+            'ID': 0
+        }
 
-    # def test_post_json_no_auth_schema_neg(self):
-    #     payload = {
-    #         'ID': 0,
-    #         'superfluous': "This is one more argument than the openapi description accepts"
-    #     }
-    #
-    #     r = requests.post('https://europe-west1-' + self._domain + '.cloudfunctions.net/' + self._domain + '-receive-ingest-func', params=params, json=payload)
+        r = requests.post('https://europe-west1-' + self._domain + '.cloudfunctions.net/' + self._domain +
+                          '-receive-ingest-func/store-json', json=payload)
+        try:
+            self.assertFalse(199 < r.status_code < 300)
+        except AssertionError as e:
+            raise type(e)(str(e) + "\n\n Full response:\n" + r.text)
 
-    #     try:
-    #         self.assertFalse(199 < r.status_code < 300)
-    #     except AssertionError as e:
-    #         import sys
-    #         raise type(e)(str(e) + "\n\n Full response:\n" + r.text).with_traceback(sys.exc_info()[2])
+    def test_post_json_no_auth_schema_neg(self):
+        payload = {
+            'ID': 0,
+            'superfluous': "This is one more argument than the openapi description accepts"
+        }
+
+        r = requests.post('https://europe-west1-' + self._domain + '.cloudfunctions.net/' + self._domain +
+                          '-receive-ingest-func/store-json', json=payload)
+        try:
+            self.assertFalse(199 < r.status_code < 300)
+        except AssertionError as e:
+            import sys
+            raise type(e)(str(e) + "\n\n Full response:\n" + r.text).with_traceback(sys.exc_info()[2])
