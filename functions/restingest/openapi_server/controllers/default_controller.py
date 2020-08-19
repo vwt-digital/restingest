42a2a02475021b44ceeb49e57fa2e844aaae2c0f
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-08-19 13:12:32 +0200

    Testing namespace removal without defusedxml DAT-3769

--- functions/restingest/openapi_server/controllers/default_controller.py
+++ functions/restingest/openapi_server/controllers/default_controller.py
@@ -9,7 +9,6 @@ from flask import make_response
 from flask import current_app
 from flask import request
 
-from defusedxml import ElementTree as defusedxml_ET
 from lxml import etree as ET  # nosec
 
 
@@ -36,8 +35,7 @@ def apply_pii_filter(body, pii_filter):
 def store_blobs(destination_path, blob_data, content_type, should_apply_pii_filter):
     if content_type in ['text/xml', 'application/xml', 'application/xml-external-parsed-entity',
                         'text/xml-external-parsed-entity', 'application/xml-dtd']:
-        safe_xml_tree = defusedxml_ET.fromstring(blob_data)
-        xml_tree = ET.fromstring(defusedxml_ET.tostring(safe_xml_tree))  # nosec
+        xml_tree = ET.fromstring(blob_data)  # nosec
 
         for elem in xml_tree.getiterator():
             elem.tag = ET.QName(elem).localname
