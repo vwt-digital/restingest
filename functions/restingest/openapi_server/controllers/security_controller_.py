1a6c8e483e2f6f7e1a71d2b27e1bd22295e756e5
Bernie van Veen <b.vanveen@vwt.digital>
2020-11-24 16:58:22 +0100

    Added api key authentication for receive endpoint. DAT-4842

--- functions/restingest/openapi_server/controllers/security_controller_.py
+++ functions/restingest/openapi_server/controllers/security_controller_.py
@@ -1,12 +1,20 @@
 import config
+import logging
+import os
+from utils import get_secret
+from flask import g
 from jwkaas import JWKaas
 
 my_jwkaas = None
+api_key_secret = None
 
 if hasattr(config, 'OAUTH_JWKS_URL'):
     my_jwkaas = JWKaas(config.OAUTH_EXPECTED_AUDIENCE,
                        config.OAUTH_EXPECTED_ISSUER,
                        jwks_url=config.OAUTH_JWKS_URL)
+
+if hasattr(config, 'API_KEY_SECRET'):
+    api_key_secret = get_secret(os.environ['PROJECT_ID'], config.API_KEY_SECRET)
 
 
 def refine_token_info(token_info):
@@ -29,3 +37,13 @@ def info_from_oauth2(token):
     :rtype: dict | None
     """
     return refine_token_info(my_jwkaas.get_token_info(token))
+
+
+def info_from_apikey(apikey, required_scopes):
+    if not api_key_secret:
+        logging.error('API Key authorization configured but API key secret is missing from config.')
+    elif apikey == api_key_secret:
+        g.user = 'apikeyuser'
+        return {'sub': 'apikeyuser'}
+
+    return None
