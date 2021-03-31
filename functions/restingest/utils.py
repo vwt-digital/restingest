71b83bba41ae5cdf1696941ef566a5f21fb69975
Eric van der Velde <e.vandervelde@vwt.digital>
2021-03-31 10:28:22 +0200

    Bumped versions. DAT-6155

--- functions/restingest/utils.py
+++ functions/restingest/utils.py
@@ -1,29 +1,25 @@
 import base64
+
 import requests
-
-from google.cloud import kms_v1
-from google.cloud import secretmanager_v1
-
+from google.cloud import kms_v1, secretmanager_v1
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
 
-def decrypt_secret(project_id, kms_region, kms_keyring, kms_key, encrypted_secret_base64):
+def decrypt_secret(
+    project_id, kms_region, kms_keyring, kms_key, encrypted_secret_base64
+):
     """
     Returns a KMS decrypted secret.
     """
 
     client = kms_v1.KeyManagementServiceClient()
 
-    key_name = client.crypto_key_path_path(
-        project_id,
-        kms_region,
-        kms_keyring,
-        kms_key)
+    key_name = client.crypto_key_path_path(project_id, kms_region, kms_keyring, kms_key)
 
     encrypted_secret = base64.b64decode(encrypted_secret_base64)
     decrypt_response = client.decrypt(key_name, encrypted_secret)
-    payload = decrypt_response.plaintext.decode("utf-8").replace('\n', '')
+    payload = decrypt_response.plaintext.decode("utf-8").replace("\n", "")
 
     return payload
 
@@ -35,13 +31,10 @@ def get_secret(project_id, secret_id):
 
     client = secretmanager_v1.SecretManagerServiceClient()
 
-    secret_name = client.secret_version_path(
-        project_id,
-        secret_id,
-        'latest')
+    secret_name = client.secret_version_path(project_id, secret_id, "latest")
 
-    response = client.access_secret_version(secret_name)
-    payload = response.payload.data.decode('UTF-8')
+    response = client.access_secret_version(request={"name": secret_name})
+    payload = response.payload.data.decode("UTF-8")
 
     return payload
 
@@ -61,7 +54,7 @@ def get_requests_session(retries=3, backoff=1, status_forcelist=(500, 502, 503, 
     )
 
     adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
 
     return session
